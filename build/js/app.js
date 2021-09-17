b92783bade6c80fcf6c361b748ec8581165be13a
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-09-17 14:35:00 +0300

    demo

---
+++ build/js/app.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//=require ../blocks/**/*.js
+$(document).ready(function () {
+  // open modal
+  $('.js-open-swap').click(function () {
+    $('.js-modal-swap').fadeIn();
+    $('.js-mask').fadeIn();
+    $('body').addClass('overflov');
+  }); //close modal
+
+  $('.js-mask, .js-modal-close').click(function () {
+    $('.js-modal').fadeOut();
+    $('.js-mask').fadeOut();
+    $('body').removeClass('overflov');
+  });
+  var swiper = new Swiper(".swiper", {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction"
+    },
+    loop: true,
+    autoplay: true,
+    navigation: {
+      nextEl: ".slider-button-next"
+    },
+    mousewheel: true,
+    keyboard: true
+  });
+  var swiperTechnogical = new Swiper(".swiper-technogical", {
+    slidesPerView: 3,
+    spaceBetween: 32,
+    loop: false,
+    autoplay: false,
+    navigation: {
+      nextEl: ".slider-technogical-button-next"
+    },
+    pagination: {
+      clickable: true
+    },
+    mousewheel: true,
+    keyboard: true
+  });
+});
